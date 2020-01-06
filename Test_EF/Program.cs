4341a83b6a03da4a224a1bbff5f19568c0ebe347
manu <manu9576@hotmail.fr>
2020-01-06 22:55:12 +0100

    Adding code for testing

--- Test_EF/Program.cs
+++ Test_EF/Program.cs
@@ -1,4 +1,7 @@
-﻿using System;
+﻿using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
 
 namespace Test_EF
 {
@@ -7,6 +10,66 @@ namespace Test_EF
         static void Main(string[] args)
         {
             Console.WriteLine("Hello World!");
+
+            using (var db = new BloggingContext())
+            {
+                // Create
+                Console.WriteLine("Inserting a new blog");
+                db.Add(new Blog { Url = "http://blogs.msdn.com/adonet" });
+                db.SaveChanges();
+
+                // Read
+                Console.WriteLine("Querying for a blog");
+                var blog = db.Blogs
+                    .OrderBy(b => b.BlogId)
+                    .First();
+
+                // Update
+                Console.WriteLine("Updating the blog and adding a post");
+                blog.Url = "https://devblogs.microsoft.com/dotnet";
+                blog.Posts.Add(
+                    new Post
+                    {
+                        Title = "Hello World",
+                        Content = "I wrote an app using EF Core!"
+                    });
+                db.SaveChanges();
+
+                // Delete
+                Console.WriteLine("Delete the blog");
+                db.Remove(blog);
+                db.SaveChanges();
+            }
         }
     }
+
+    public class Blog
+    {
+        public int BlogId { get; set; }
+        public string Url { get; set; }
+
+        public List<Post> Posts { get; } = new List<Post>();
+    }
+
+    public class Post
+    {
+        public int PostId { get; set; }
+        public string Title { get; set; }
+        public string Content { get; set; }
+
+        public int BlogId { get; set; }
+        public Blog Blog { get; set; }
+    }
+
+    public class BloggingContext : DbContext
+    {
+        public DbSet<Blog> Blogs { get; set; }
+        public DbSet<Post> Posts { get; set; }
+
+        protected override void OnConfiguring(DbContextOptionsBuilder options)
+        {
+            options.UseMySql("Server=rasp4;User Id=root;Password=password;Database=mariadbtest");
+        }
+    }
+
 }
